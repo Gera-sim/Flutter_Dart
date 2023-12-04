afc65da2452f3c4f5a4cbc1ff5c8577e34f7ab19
Gerasim <g-926@ya.ru>
2023-12-04 15:29:16 +0300

    2.1

---
+++ lib/pages/vehicle/vehicle_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../design/colors.dart';
+import '../../design/dimensions.dart';
+
+class VehiclePage extends StatelessWidget {
+  const VehiclePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dispatcher by Gerhard.com',
+            style: TextStyle(
+              color: primaryColor,
+              fontSize: fontSize16,
+              fontWeight: FontWeight.w500,
+            )),
+        centerTitle: true,
+        elevation: elevation0, // shadow
+        backgroundColor: surfaceColor,
+      ),
+      body: Container(
+        color: backgroundColor,
+        child: const Text('BodyHolder'),
+      ),
+    );
+  }
+}
