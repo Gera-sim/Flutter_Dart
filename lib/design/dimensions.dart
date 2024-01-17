60454ebf85975656da65f6cddbe6b38107ede3db
Gerasim <g-926@ya.ru>
2024-01-17 09:32:06 +0300

    5.1 Виджет Text и Rich Text

--- lib/design/dimensions.dart
+++ lib/design/dimensions.dart
@@ -1,7 +1,10 @@
 const double elevation0 = 0;
 const double elevation006 = 0.06;
 const double height8 = 8;
+const double fontSize12 = 12;
+const double fontSize14 = 14;
 const double fontSize16 = 16;
+const double padding6 = 6;
 const double padding8 = 8;
 const double padding16 = 16;
 const double height64 = 64;
